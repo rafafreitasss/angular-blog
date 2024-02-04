f82d3472e4d429e471ca9ed05dd7e733eac3a705
rafafreitasss <rafaelfreitas97@yahoo.com>
2024-02-04 18:51:03 -0300

    portfolio
    comecei a transformação do blog em portfolio

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,8 @@
 export const dataFake = [
     {
         "id": '1',
-        "title": 'NOVO HOMEM DE FERRO ANUNCIADO',
-        "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perferendis dolores modi corrupti soluta, doloremque facilis asperiores omnis maiores, vero placeat voluptates incidunt minima fugiat repellendus rerum. Sed, voluptatibus fuga.',
+        "title": 'NOVO HOMEM DE FERRO',
+        "description": 'Olá! Me chamo Rafael e sou formado em História pela Universidade Federal de Pelotas (UFPEL). Durante a graduação aprendi a fazer diagramação e ilustração para criar meus materiais didáticos. Dessa forma, tenho domínio de ferramentas de design gráfico, como InDesign e Illustrator, entre outros programas. Estou aqui oferecendo toda a experiência que adquiri neste período, tanto na área de design quanto em relação a escrita acadêmica e redação de textos. Já realizei trabalhos publicitários, de editorial, produção de livros e e-books. Espero poder ajudar :)',
         "photoCover": 'https://th.bing.com/th/id/R.7bf4f5ca5cfd13c02af4f13182e30556?rik=QTLzFMwuKo0Ftg&pid=ImgRaw&r=0'
     },
     {
