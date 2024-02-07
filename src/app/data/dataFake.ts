cf1be32c413130ddd609ee9dfbdb7fc1d4badb93
rafafreitasss <rafaelfreitas97@yahoo.com>
2024-02-07 17:27:56 -0300

    pquenas tentativas de alteração

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,20 @@
 export const dataFake = [
     {
         "id": '1',
-        "title": 'NOVO HOMEM DE FERRO',
+        "title": 'UM POUCO SOBRE MIM',
         "description": 'Olá! Me chamo Rafael e sou formado em História pela Universidade Federal de Pelotas (UFPEL). Durante a graduação aprendi a fazer diagramação e ilustração para criar meus materiais didáticos. Dessa forma, tenho domínio de ferramentas de design gráfico, como InDesign e Illustrator, entre outros programas. Estou aqui oferecendo toda a experiência que adquiri neste período, tanto na área de design quanto em relação a escrita acadêmica e redação de textos. Já realizei trabalhos publicitários, de editorial, produção de livros e e-books. Espero poder ajudar :)',
-        "photoCover": 'https://th.bing.com/th/id/R.7bf4f5ca5cfd13c02af4f13182e30556?rik=QTLzFMwuKo0Ftg&pid=ImgRaw&r=0'
+        "photoCover": '../assets/imgs/eu.jpg'
     },
     {
         "id": '2',
-        "title": 'Nova série Marvel anunciada para 2024',
-        "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde perferendis dolores modi corrupti soluta, doloremque facilis asperiores omnis maiores, vero placeat voluptates incidunt minima fugiat repellendus rerum. Sed, voluptatibus fuga.',
-        "photoCover": 'https://cdn-images-1.medium.com/max/1600/0*A3dtHF1Pk8_VgmWJ.jpeg'
+        "title": 'Controle a sua raiva e cure a sua vida',
+        "description": '',
+        "photoCover": '../assets/imgs/controle-sua-raiva.png'
+    },
+    {
+        "id": '3',
+        "title": 'Tá Bonito pra Chover',
+        "description": '',
+        "photoCover": '../assets/imgs/mockup.jpg'
     }
 ]
